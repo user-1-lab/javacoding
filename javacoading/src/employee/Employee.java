65eb0d0323737d92423fa0047b8ba1f95d0141dd
Mohana Prabanjan <mohanaprabanjan16@gmail.com>
2024-07-29 13:46:43 +0530

    encapsulation

---
+++ javacoading/src/employee/Employee.java
@@ -0,0 +1,40 @@
+package employee;
+
+public class Employee {
+	private int eid;
+	private String ename;
+	private double salary;
+	private String city;
+	public int getEid() 
+	{
+		return eid;
+	}
+	public void setEid(int eid) 
+	{
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeDetails [eid=" + eid + ", ename=" + ename + ", salary=" + salary + ", city=" + city + "]";
+	}
+	
+
+}
---
+++ javacoading/src/employee/EmployeeDemo.java
@@ -0,0 +1,28 @@
+package employee;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+     	e1.setEid(101); 
+		e1.setEname("Mohan");
+		e1.setSalary(200000.0d);
+		e1.setCity("Puducherry");
+		
+		System.out.println(e1);
+		Employee e2=new Employee();
+     	e2.setEid(101); 
+		e2.setEname("Mohan");
+		e2.setSalary(200000.0d);
+		e2.setCity("Puducherry");
+		
+		System.out.println(e2);
+		Employee e3=new Employee();
+     	e3.setEid(101); 
+		e3.setEname("Mohan");
+		e3.setSalary(200000.0d);
+		e3.setCity("Puducherry");
+		
+		System.out.println(e3);
+		}
+}
